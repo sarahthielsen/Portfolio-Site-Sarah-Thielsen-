62354f1af55a3dce43a750059e16e3acfb0c3ae8
sarahthielsen <134093184+sarahthielsen@users.noreply.github.com>
2023-07-01 15:58:34 -0600

    create interactive navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hey! Welcome to my portfolio site!")
+function menuToggle() {
+    var x =document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+    if(x.className === 'navToggle') {  //browser checks if class name of x is navToggle, if yes - it adds responsive to className
+    x.className += ' responsive';
+    } else {
+        x.className = 'navToggle';
+    }
+}
