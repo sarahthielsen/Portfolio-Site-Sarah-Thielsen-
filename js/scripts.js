faa73b4d4df5d4fbd7176da491bf66b6a0fbb038
sarahthielsen <134093184+sarahthielsen@users.noreply.github.com>
2023-07-04 21:12:14 -0600

    lol

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 console.log("Hey! Welcome to my portfolio site!")
+
 function menuToggle() {
-    var x =document.getElementById('myNavtoggle');
-    x.className += ' responsive';
-    if(x.className === 'navToggle') {  //browser checks if class name of x is navToggle, if yes - it adds responsive to className
-    x.className += ' responsive';
+    var x = document.getElementById('myNavtoggle');
+      var isMobileBreakpoint = window.innerWidth <= 641
+  
+    if (x.className === 'navtoggle' && isMobileBreakpoint) {
+      x.className += ' responsive';
     } else {
-        x.className = 'navToggle';
+      x.className = 'navtoggle';
     }
-}
+  }
